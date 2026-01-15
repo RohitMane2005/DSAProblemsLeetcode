bfb5dfcaaa1e84b8a220245936b99bbdd4974ff0
Rohit Mane <rohitbusiness9115@gmail.com>
2026-01-15 15:03:02 +0000

    Add initial approach for Wiggle Sort II
    Problem:      https://leetcode.com/problems/wiggle-sort-ii/
    LeetCode-ID:  #0324 (java)
    Runtime:      10 ms
    Memory:       48.9 MB
    Submitted:    2026-01-15 15:44:09 UTC

--- Uncategorized/LeetCode_0324_Wiggle_Sort_II/solution.java
+++ Uncategorized/LeetCode_0324_Wiggle_Sort_II/solution.java
@@ -6,11 +6,3 @@ class Solution {
         int mid = (n+1)/2-1;
         int end = n-1;
         for(int i = 0; i<n; i++){
-            if(i % 2 == 0){
-                arr[i] = sorted[mid--];
-            }else{
-                arr[i] = sorted[end--];
-            }
-        }
-    }
-}
