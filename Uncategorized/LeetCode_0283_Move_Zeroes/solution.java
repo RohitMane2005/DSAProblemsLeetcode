3d40c6d602944c646ae22e1020af81fabd4e981c
Rohit Mane <rohitbusiness9115@gmail.com>
2026-01-18 14:41:33 +0000

    Draft initial solution for Move Zeroes
    Problem:      https://leetcode.com/problems/move-zeroes/
    LeetCode-ID:  #0283 (java)
    Runtime:      2 ms
    Memory:       48.1 MB
    Submitted:    2026-01-18 15:14:16 UTC

--- Uncategorized/LeetCode_0283_Move_Zeroes/solution.java
+++ Uncategorized/LeetCode_0283_Move_Zeroes/solution.java
@@ -4,10 +4,3 @@ class Solution {
         for(int i = 0; i<arr.length; i++){
             if(arr[i] != 0){
                 int temp = arr[i];
-                arr[i] = arr[zeros];
-                arr[zeros] = temp;
-                zeros++;
-            } 
-        }
-    }
-}
