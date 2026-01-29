0c7d9a47e605f8d490020b592d18995af28d5a0b
Rohit Mane <rohitbusiness9115@gmail.com>
2026-01-29 13:36:28 +0000

    Refactor Pascal's Triangle II for better runtime performance
    Problem:      https://leetcode.com/problems/pascals-triangle-ii/
    LeetCode-ID:  #0119 (java)
    Runtime:      2 ms
    Memory:       42.8 MB
    Submitted:    2026-01-29 13:49:01 UTC

--- Uncategorized/LeetCode_0119_Pascals_Triangle_II/solution.java
+++ Uncategorized/LeetCode_0119_Pascals_Triangle_II/solution.java
@@ -9,3 +9,14 @@ class Solution {
                 if (j == 0 || j == i) {
                     row.add(1);
                 } else {
+                    int val = ans.get(i - 1).get(j - 1)
+                            + ans.get(i - 1).get(j);
+                    row.add(val);
+                }
+            }
+            ans.add(row);
+        }
+        return ans.get(rowIndex);
+
+    }
+}
