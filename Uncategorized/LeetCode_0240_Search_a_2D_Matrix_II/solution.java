6215d4386f48999fe945372138e1473da3e05a7b
Rohit Mane <rohitbusiness9115@gmail.com>
2026-01-29 14:05:44 +0000

    Format code and add problem notes for Search a 2D Matrix II
    Problem:      https://leetcode.com/problems/search-a-2d-matrix-ii/
    LeetCode-ID:  #0240 (java)
    Runtime:      3 ms
    Memory:       48.3 MB
    Submitted:    2026-01-29 14:05:44 UTC

--- Uncategorized/LeetCode_0240_Search_a_2D_Matrix_II/solution.java
+++ Uncategorized/LeetCode_0240_Search_a_2D_Matrix_II/solution.java
@@ -8,3 +8,13 @@ class Solution {
 
         while (i < m && j >= 0) {
             if (arr[i][j] == target) {
+                return true;
+            } else if (arr[i][j] > target) {
+                j--;   // move left
+            } else {
+                i++;   // move down
+            }
+        }
+        return false;
+    }
+}
