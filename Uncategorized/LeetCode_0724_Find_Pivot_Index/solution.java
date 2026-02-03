621712c3494df068aa43dfbca577c763f78e1d9c
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-03 13:37:27 +0000

    Initial solution implementation for #0724
    Problem:      https://leetcode.com/problems/find-pivot-index/
    LeetCode-ID:  #0724 (java)
    Runtime:      1 ms
    Memory:       47.7 MB
    Submitted:    2026-02-03 14:13:22 UTC

--- Uncategorized/LeetCode_0724_Find_Pivot_Index/solution.java
+++ Uncategorized/LeetCode_0724_Find_Pivot_Index/solution.java
@@ -7,13 +7,3 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             totalSum -= nums[i];   // right sum
-
-            if (leftSum == totalSum) {
-                return i;
-            }
-
-            leftSum += nums[i];
-        }
-        return -1;
-    }
-}
