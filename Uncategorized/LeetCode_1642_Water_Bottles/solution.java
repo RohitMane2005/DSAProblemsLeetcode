cb83ae23f8542ad7dec37552f9a13e242fc79a21
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-10 13:44:50 +0000

    Initial solution implementation for #1642
    Problem:      https://leetcode.com/problems/water-bottles/
    LeetCode-ID:  #1642 (java)
    Runtime:      0 ms
    Memory:       42.2 MB
    Submitted:    2026-02-10 14:16:52 UTC

--- Uncategorized/LeetCode_1642_Water_Bottles/solution.java
+++ Uncategorized/LeetCode_1642_Water_Bottles/solution.java
@@ -4,10 +4,3 @@ class Solution {
         // while(numBottles >= numExchange){
         // int divide = numBottles/numExchange;
         // int remeaning = numBottles%numExchange;
-        // maxNum+=divide;
-        // numBottles = divide + remeaning;
-        // }
-        // return maxNum;
-         return numBottles + (numBottles - 1) / (numExchange - 1);
-    }
-}
