d2449e2d9bbb3a7950092f36cd8d14ffe41146aa
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-12 15:07:48 +0000

    Finalize Median of Two Sorted Arrays solution (java)
    Problem:      https://leetcode.com/problems/median-of-two-sorted-arrays/
    LeetCode-ID:  #0004 (java)
    Runtime:      2 ms
    Memory:       48.5 MB
    Submitted:    2026-02-12 15:07:48 UTC

--- Uncategorized/LeetCode_0004_Median_of_Two_Sorted_Arrays/solution.java
+++ Uncategorized/LeetCode_0004_Median_of_Two_Sorted_Arrays/solution.java
@@ -15,3 +15,21 @@ class Solution {
         int p1 = 0, p2 = 0, index = 0;
 
         while (p1 < arr1.length && p2 < arr2.length) {
+            if (arr1[p1] < arr2[p2]) {
+                ans[index++] = arr1[p1++];
+            } else {
+                ans[index++] = arr2[p2++];
+            }
+        }
+
+        while (p1 < arr1.length) {
+            ans[index++] = arr1[p1++];
+        }
+
+        while (p2 < arr2.length) {
+            ans[index++] = arr2[p2++];
+        }
+
+        return ans;
+    }
+}
