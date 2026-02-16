c9449458fb708ea077f4381889b3644ecddc59be
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-16 14:39:45 +0000

    Setup solution structure for Largest Number At Least Twice of Others
    Problem:      https://leetcode.com/problems/largest-number-at-least-twice-of-others/
    LeetCode-ID:  #0748 (java)
    Runtime:      0 ms
    Memory:       43.5 MB
    Submitted:    2026-02-16 15:16:54 UTC

--- Uncategorized/LeetCode_0748_Largest_Number_At_Least_Twice_of_Others/solution.java
+++ Uncategorized/LeetCode_0748_Largest_Number_At_Least_Twice_of_Others/solution.java
@@ -8,14 +8,3 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > max) {
-                secondMax = max;
-                max = nums[i];
-                index = i;
-            } else if (nums[i] > secondMax) {
-                secondMax = nums[i];
-            }
-        }
-
-        return max >= 2 * secondMax ? index : -1;
-    }
-}
