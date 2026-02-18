8c643289235413d94c0f51f44069b5d12cfbfb51
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-18 15:19:28 +0000

    Finalize Maximum Ascending Subarray Sum solution (java)
    Problem:      https://leetcode.com/problems/maximum-ascending-subarray-sum/
    LeetCode-ID:  #1927 (java)
    Runtime:      0 ms
    Memory:       43.3 MB
    Submitted:    2026-02-18 15:19:28 UTC

--- Uncategorized/LeetCode_1927_Maximum_Ascending_Subarray_Sum/solution.java
+++ Uncategorized/LeetCode_1927_Maximum_Ascending_Subarray_Sum/solution.java
@@ -6,3 +6,12 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] > nums[i - 1]) {
                 currentSum += nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
