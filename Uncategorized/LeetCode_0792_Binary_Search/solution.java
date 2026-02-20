c3cd458be951d53f99662756188024e9afd25645
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-20 16:08:02 +0000

    docs(0792): add complexity notes and finalize Binary Search
    Problem:      https://leetcode.com/problems/binary-search/
    LeetCode-ID:  #0792 (java)
    Runtime:      0 ms
    Memory:       48.2 MB
    Submitted:    2026-02-20 16:08:02 UTC

--- Uncategorized/LeetCode_0792_Binary_Search/solution.java
+++ Uncategorized/LeetCode_0792_Binary_Search/solution.java
@@ -6,3 +6,12 @@ class Solution {
             int mid = left + (right - left )/2;
             if(nums[mid] == target){
                 return mid;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
