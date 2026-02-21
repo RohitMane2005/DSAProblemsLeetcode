31f2904cc14d7c2dd011de408e1247346fe8c3c2
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-21 14:24:04 +0000

    Draft initial solution for Find Peak Element
    Problem:      https://leetcode.com/problems/find-peak-element/
    LeetCode-ID:  #0162 (java)
    Runtime:      0 ms
    Memory:       44.4 MB
    Submitted:    2026-02-21 14:48:22 UTC

--- Uncategorized/LeetCode_0162_Find_Peak_Element/solution.java
+++ Uncategorized/LeetCode_0162_Find_Peak_Element/solution.java
@@ -5,11 +5,3 @@ class Solution {
         while(left < right){
             int mid = left + (right - left)/2;
            if(arr[mid] < arr[mid+1]){
-            left = mid + 1;
-           }else{
-            right = mid;
-           }
-        }
-        return left;
-    }
-}
