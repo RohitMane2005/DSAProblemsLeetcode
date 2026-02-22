e5365bd395a149aad26efaee49e4ff3493f56b6d
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-22 14:34:38 +0000

    [LeetCode] #0540 Single Element in a Sorted Array (java)
    Problem:   https://leetcode.com/problems/single-element-in-a-sorted-array/
    Runtime:   0 ms
    Memory:    53.1 MB
    Submitted: 2026-02-22 14:34:38 UTC

---
+++ Uncategorized/LeetCode_0540_Single_Element_in_a_Sorted_Array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        int left = 0;
+        int right = arr.length -1;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(mid%2 == 1) mid--;
+            if(arr[mid] == arr[mid+1]){
+                left = mid + 2;
+            }else{
+                right = mid;
+            }
+        }
+        return arr[left];
+    }
+}
