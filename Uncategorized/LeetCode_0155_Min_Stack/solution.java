3c1cb83095a1e1418ffe3dd945358763a4c7d56b
Rohit Mane <rohitbusiness9115@gmail.com>
2026-02-25 13:36:32 +0000

    Setup solution structure for Min Stack
    Problem:      https://leetcode.com/problems/min-stack/
    LeetCode-ID:  #0155 (java)
    Runtime:      5 ms
    Memory:       47.2 MB
    Submitted:    2026-02-25 14:07:04 UTC

--- Uncategorized/LeetCode_0155_Min_Stack/solution.java
+++ Uncategorized/LeetCode_0155_Min_Stack/solution.java
@@ -20,25 +20,3 @@ class MinStack {
 
     public void pop() {
         if (mainStack.peek().equals(minStack.peek())) {
-            minStack.pop();
-        }
-        mainStack.pop();
-    }
-
-    public int top() {
-        return mainStack.peek();
-    }
-
-    public int getMin() {
-        return minStack.peek();
-    }
-}
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
