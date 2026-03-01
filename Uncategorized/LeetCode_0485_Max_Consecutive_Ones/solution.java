fdc44003efd098ab325dbedba9350645f51a988a
Rohit Mane <rohitbusiness9115@gmail.com>
2026-03-01 14:35:10 +0000

    Optimize solution using efficient data structures
    Problem:      https://leetcode.com/problems/max-consecutive-ones/
    LeetCode-ID:  #0485 (java)
    Runtime:      3 ms
    Memory:       52.7 MB
    Submitted:    2026-03-01 14:46:39 UTC

--- Uncategorized/LeetCode_0485_Max_Consecutive_Ones/solution.java
+++ Uncategorized/LeetCode_0485_Max_Consecutive_Ones/solution.java
@@ -5,3 +5,11 @@ class Solution {
         for(int x : nums){
             if(x == 1){
                 count++;
+                max = Math.max(max, count);
+            }else{
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
