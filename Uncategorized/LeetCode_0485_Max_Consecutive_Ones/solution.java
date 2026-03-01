3f81a9d56dacc680f430057a42b1301ee3e4a7cc
Rohit Mane <rohitbusiness9115@gmail.com>
2026-03-01 14:01:24 +0000

    feat(0485): draft initial solution for Max Consecutive Ones
    Problem:      https://leetcode.com/problems/max-consecutive-ones/
    LeetCode-ID:  #0485 (java)
    Runtime:      3 ms
    Memory:       52.7 MB
    Submitted:    2026-03-01 14:46:39 UTC

--- Uncategorized/LeetCode_0485_Max_Consecutive_Ones/solution.java
+++ Uncategorized/LeetCode_0485_Max_Consecutive_Ones/solution.java
@@ -5,11 +5,3 @@ class Solution {
         for(int x : nums){
             if(x == 1){
                 count++;
-                max = Math.max(max, count);
-            }else{
-                count = 0;
-            }
-        }
-        return max;
-    }
-}
