65251eb420ba2ac24368475ab83fe8e6d0742cd8
Rohit Mane <rohitbusiness9115@gmail.com>
2026-03-02 14:53:25 +0000

    Finalize Set Mismatch solution (java)
    Problem:      https://leetcode.com/problems/set-mismatch/
    LeetCode-ID:  #0645 (java)
    Runtime:      3 ms
    Memory:       47.3 MB
    Submitted:    2026-03-02 14:53:25 UTC

--- Uncategorized/LeetCode_0645_Set_Mismatch/solution.java
+++ Uncategorized/LeetCode_0645_Set_Mismatch/solution.java
@@ -10,3 +10,16 @@ class Solution {
                 duplicate = Math.abs(nums[i]);
             } else {
                 nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
