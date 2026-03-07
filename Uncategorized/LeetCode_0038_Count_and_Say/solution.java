d30eff8aba81cc7b460135775eb62eb7b0b06b54
Rohit Mane <rohitbusiness9115@gmail.com>
2026-03-07 07:30:22 +0000

    Draft initial solution for Count and Say
    Problem:      https://leetcode.com/problems/count-and-say/
    LeetCode-ID:  #0038 (java)
    Runtime:      18 ms
    Memory:       52.8 MB
    Submitted:    2026-03-07 07:58:30 UTC

--- Uncategorized/LeetCode_0038_Count_and_Say/solution.java
+++ Uncategorized/LeetCode_0038_Count_and_Say/solution.java
@@ -7,12 +7,3 @@ class Solution {
         while(j != s.length()){
             if(s.charAt(i) == s.charAt(j)) j++;
             else{
-                int freq = j - i;
-                ans += freq;
-                ans += s.charAt(i);
-                i = j;
-            }
-        } 
-        return ans;
-    }
-}
