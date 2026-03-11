6e60147ca4f848ca40d0cf34f3f6cd1b5b0f8194
Rohit Mane <rohitbusiness9115@gmail.com>
2026-03-11 15:14:02 +0000

    feat(2107): draft initial solution for Find Unique Binary String
    Problem:      https://leetcode.com/problems/find-unique-binary-string/
    LeetCode-ID:  #2107 (java)
    Runtime:      0 ms
    Memory:       43.3 MB
    Submitted:    2026-03-11 15:53:47 UTC

--- Uncategorized/LeetCode_2107_Find_Unique_Binary_String/solution.java
+++ Uncategorized/LeetCode_2107_Find_Unique_Binary_String/solution.java
@@ -7,13 +7,3 @@ class Solution {
         for (int i = 0; i < n; i++) {
 
             char c = nums[i].charAt(i);
-
-            if (c == '0')
-                ans.append('1');
-            else
-                ans.append('0');
-        }
-
-        return ans.toString();
-    }
-}
