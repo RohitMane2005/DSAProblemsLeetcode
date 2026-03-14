4fd09893e1e7da7692e874d8eed54d8e7b14eb3b
Rohit Mane <rohitbusiness9115@gmail.com>
2026-03-14 14:55:44 +0000

    [LeetCode] #0442 Find All Duplicates in an Array (java)
    Problem:   https://leetcode.com/problems/find-all-duplicates-in-an-array/
    Runtime:   6 ms
    Memory:    57.7 MB
    Submitted: 2026-03-14 14:55:44 UTC

---
+++ Uncategorized/LeetCode_0442_Find_All_Duplicates_in_an_Array/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+        int i = 0;
+        while(i < nums.length){
+            int right = nums[i] - 1;
+
+            if(nums[i] != nums[right]){
+                int temp = nums[i];
+                nums[i] = nums[right];
+                nums[right] = temp;
+            }else{
+                i++;
+            }
+        }
+
+        for(i = 0; i < nums.length; i++){
+            if(nums[i] != i + 1){
+                ans.add(nums[i]);
+            }
+        }
+
+        return ans;
+    }
+}
