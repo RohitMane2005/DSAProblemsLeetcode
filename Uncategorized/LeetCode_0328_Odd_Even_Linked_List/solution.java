e33933bdb581a4ffadac6b3c06bb90feabc04525
Rohit Mane <rohitbusiness9115@gmail.com>
2026-04-13 14:30:24 +0000

    Clean up solution and add comments for #0328
    Problem:      https://leetcode.com/problems/odd-even-linked-list/
    LeetCode-ID:  #0328 (java)
    Runtime:      0 ms
    Memory:       46.4 MB
    Submitted:    2026-04-13 14:30:24 UTC

--- Uncategorized/LeetCode_0328_Odd_Even_Linked_List/solution.java
+++ Uncategorized/LeetCode_0328_Odd_Even_Linked_List/solution.java
@@ -8,3 +8,14 @@ class Solution {
 
         while (even != null && even.next != null) {
             odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+    }
+}
