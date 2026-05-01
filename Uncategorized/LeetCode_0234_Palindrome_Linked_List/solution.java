f05502bca13431378fd943cad805d3d525e1238a
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-01 09:11:20 +0000

    feat(0234): draft initial solution for Palindrome Linked List
    Problem:      https://leetcode.com/problems/palindrome-linked-list/
    LeetCode-ID:  #0234 (java)
    Runtime:      4 ms
    Memory:       94.2 MB
    Submitted:    2026-05-01 09:41:31 UTC

--- Uncategorized/LeetCode_0234_Palindrome_Linked_List/solution.java
+++ Uncategorized/LeetCode_0234_Palindrome_Linked_List/solution.java
@@ -22,27 +22,3 @@ class Solution {
 
         while(fast != null && fast.next != null){
             slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // 🔥 Skip middle for odd length
-        if(fast != null){ // odd length
-            slow = slow.next;
-        }
-
-        // Reverse second half
-        ListNode secondHalf = reverse(slow);
-        ListNode firstHalf = head;
-
-        // Compare
-        while(secondHalf != null){
-            if(firstHalf.val != secondHalf.val){
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-}
