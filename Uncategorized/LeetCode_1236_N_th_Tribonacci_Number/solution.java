913e4b9665a251c1fe627fe9fc9154ceb3905dcb
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-02 10:44:10 +0000

    [LeetCode] #1236 N-th Tribonacci Number (java)
    Problem:   https://leetcode.com/problems/n-th-tribonacci-number/
    Runtime:   0 ms
    Memory:    42 MB
    Submitted: 2026-05-02 10:44:10 UTC

---
+++ Uncategorized/LeetCode_1236_N_th_Tribonacci_Number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        int first = 0;
+        int sec = 1;
+        int third = 1;
+        for(int i = 0; i<n; i++){
+            int forth = first + sec + third;
+            first = sec;
+            sec = third;
+            third = forth;
+
+        }
+        return first;
+    }
+}
