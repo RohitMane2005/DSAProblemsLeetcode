ed5b22e81813ab8093daf5640283926140cebebf
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-04 14:46:55 +0000

    docs(1528): add complexity notes and finalize Kids With the Greatest Number of Candies
    Problem:      https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
    LeetCode-ID:  #1528 (java)
    Runtime:      1 ms
    Memory:       43.6 MB
    Submitted:    2026-05-04 14:46:55 UTC

--- Uncategorized/LeetCode_1528_Kids_With_the_Greatest_Number_of_Candies/solution.java
+++ Uncategorized/LeetCode_1528_Kids_With_the_Greatest_Number_of_Candies/solution.java
@@ -9,3 +9,16 @@ class Solution {
         for (int candy : arr) {
             maxCan = Math.max(maxCan, candy);
         }
+
+        // Step 2: Compare each kid
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] + extraCandies >= maxCan) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+
+        return ans;
+    }
+}
