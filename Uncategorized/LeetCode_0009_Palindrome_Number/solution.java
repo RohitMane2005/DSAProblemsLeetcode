81b8a725177a6c145cd175209796205262a76d9e
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-15 08:46:00 +0000

    Add complexity analysis and final tests for Palindrome Number
    Problem:      https://leetcode.com/problems/palindrome-number/
    LeetCode-ID:  #0009 (java)
    Runtime:      0 ms
    Memory:       46 MB
    Submitted:    2026-05-15 08:46:00 UTC

--- Uncategorized/LeetCode_0009_Palindrome_Number/solution.java
+++ Uncategorized/LeetCode_0009_Palindrome_Number/solution.java
@@ -10,3 +10,15 @@ class Solution {
     }
     public boolean isPalindrome(int x) {
     
+        int re=0;
+        int y=x;
+        while(x>0)
+        {
+          int a=x%10;
+          re=re*10+a;
+          x/=10;
+        }
+        
+        return y==re;
+    }
+}
