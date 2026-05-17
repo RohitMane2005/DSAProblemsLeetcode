0504d31dbed93e9404a1660a0025a19c34a4526c
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-17 14:51:32 +0000

    Clean up solution and add comments for #0076
    Problem:      https://leetcode.com/problems/minimum-window-substring/
    LeetCode-ID:  #0076 (java)
    Runtime:      16 ms
    Memory:       47.2 MB
    Submitted:    2026-05-17 14:51:32 UTC

--- Uncategorized/LeetCode_0076_Minimum_Window_Substring/solution.java
+++ Uncategorized/LeetCode_0076_Minimum_Window_Substring/solution.java
@@ -28,3 +28,33 @@ class Solution {
                 if (map.get(ch) >= 0) {
                     count++;
                 }
+            }
+
+            // Valid window found
+            while (count == t.length()) {
+
+                // Update answer
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+
+                    if (map.get(leftChar) > 0) {
+                        count--;
+                    }
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
