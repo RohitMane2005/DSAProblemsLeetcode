b8bce25dbf2f1ebac79fefa09b69945f4d2783f6
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-17 14:51:32 +0000

    [LeetCode] #0076 Minimum Window Substring (java)
    Problem:   https://leetcode.com/problems/minimum-window-substring/
    Runtime:   16 ms
    Memory:    47.2 MB
    Submitted: 2026-05-17 14:51:32 UTC

---
+++ Uncategorized/LeetCode_0076_Minimum_Window_Substring/solution.java
@@ -0,0 +1,60 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        if (s.length() < t.length()) {
+            return "";
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Store frequency of chars in t
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int left = 0;
+        int count = 0;
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+
+                if (map.get(ch) >= 0) {
+                    count++;
+                }
+            }
+
+            // Valid window found
+            while (count == t.length()) {
+
+                // Update answer
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+
+                    if (map.get(leftChar) > 0) {
+                        count--;
+                    }
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
