41871f374cddbca6ba329f5b239485c0a662990f
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-19 14:49:58 +0000

    [LeetCode] #2634 Minimum Common Value (java)
    Problem:   https://leetcode.com/problems/minimum-common-value/
    Runtime:   7 ms
    Memory:    80.3 MB
    Submitted: 2026-05-19 14:49:58 UTC

---
+++ Uncategorized/LeetCode_2634_Minimum_Common_Value/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        for(int num : nums1){
+            if(binearySearch(nums2,num)){
+                return num;
+            }
+        }
+        return -1;
+    }
+
+    public boolean binearySearch(int arr[], int num){
+        int left = 0;
+        int right = arr.length-1;
+        while(left <= right){
+            int mid = left +(right - left)/2;
+            if(arr[mid] == num){
+                return true;
+            }if(arr[mid] < num){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return false;
+    }
+}
