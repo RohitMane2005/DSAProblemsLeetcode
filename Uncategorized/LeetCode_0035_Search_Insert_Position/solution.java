c02e1444197bfbb9288b7e3d9951aefecfb475d6
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-19 14:27:11 +0000

    Clean up solution and add comments for #0035
    Problem:      https://leetcode.com/problems/search-insert-position/
    LeetCode-ID:  #0035 (java)
    Runtime:      0 ms
    Memory:       44.3 MB
    Submitted:    2026-05-19 14:27:11 UTC

--- Uncategorized/LeetCode_0035_Search_Insert_Position/solution.java
+++ Uncategorized/LeetCode_0035_Search_Insert_Position/solution.java
@@ -7,3 +7,13 @@ class Solution {
             if(nums[mid] == target){
                 return mid;
             }
+            if(nums[mid] < target){
+                left = mid +1;
+            }
+            if(nums[mid] > target){
+                right = mid -1;
+            }
+        }
+        return left;
+    }
+}
