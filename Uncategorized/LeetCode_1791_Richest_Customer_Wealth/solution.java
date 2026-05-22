9576de34000edd605d7ab7d6cf6bfd50f7e8433d
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-22 13:48:40 +0000

    Refactor Richest Customer Wealth for better runtime performance
    Problem:      https://leetcode.com/problems/richest-customer-wealth/
    LeetCode-ID:  #1791 (java)
    Runtime:      1 ms
    Memory:       44.4 MB
    Submitted:    2026-05-22 13:59:51 UTC

--- Uncategorized/LeetCode_1791_Richest_Customer_Wealth/solution.java
+++ Uncategorized/LeetCode_1791_Richest_Customer_Wealth/solution.java
@@ -4,3 +4,10 @@ class Solution {
         for(int i = 0; i<accounts.length; i++){
             int sum = 0;
             for(int j = 0; j < accounts[i].length; j++){
+                sum += accounts[i][j];
+            }
+            maxWelth = Math.max(maxWelth,sum);
+        }
+        return maxWelth;
+    }
+}
