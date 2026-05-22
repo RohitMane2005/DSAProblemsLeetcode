5f37a8171be70a8a9cd433f50e48e900c1d6296a
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-22 13:15:13 +0000

    Add initial approach for Search in Rotated Sorted Array
    Problem:      https://leetcode.com/problems/search-in-rotated-sorted-array/
    LeetCode-ID:  #0033 (java)
    Runtime:      0 ms
    Memory:       43.7 MB
    Submitted:    2026-05-22 13:45:22 UTC

--- Uncategorized/LeetCode_0033_Search_in_Rotated_Sorted_Array/solution.java
+++ Uncategorized/LeetCode_0033_Search_in_Rotated_Sorted_Array/solution.java
@@ -12,18 +12,3 @@ class Solution {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
-                    left = mid + 1;
-                }
-            }
-            // Right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-        }
-        return -1;
-    }
-}
