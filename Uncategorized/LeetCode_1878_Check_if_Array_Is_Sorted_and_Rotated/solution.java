9879eae6dd6b0e51d9ef27b863891c68d81fe817
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-23 15:05:57 +0000

    Add complexity analysis and final tests for Check if Array Is Sorted and Rotated
    Problem:      https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
    LeetCode-ID:  #1878 (java)
    Runtime:      0 ms
    Memory:       42.9 MB
    Submitted:    2026-05-23 15:05:57 UTC

--- Uncategorized/LeetCode_1878_Check_if_Array_Is_Sorted_and_Rotated/solution.java
+++ Uncategorized/LeetCode_1878_Check_if_Array_Is_Sorted_and_Rotated/solution.java
@@ -5,3 +5,10 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+}
