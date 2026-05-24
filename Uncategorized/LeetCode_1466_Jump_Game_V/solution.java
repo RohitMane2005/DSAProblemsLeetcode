2f4086c84e8443158328e26b8297ac2a7d21ad05
Rohit Mane <rohitbusiness9115@gmail.com>
2026-05-24 16:46:40 +0000

    Initial solution implementation for #1466
    Problem:      https://leetcode.com/problems/jump-game-v/
    LeetCode-ID:  #1466 (java)
    Runtime:      10 ms
    Memory:       46.5 MB
    Submitted:    2026-05-24 17:20:24 UTC

--- Uncategorized/LeetCode_1466_Jump_Game_V/solution.java
+++ Uncategorized/LeetCode_1466_Jump_Game_V/solution.java
@@ -18,24 +18,3 @@ class Solution {
         }
 
         int maxJump = 1;
-
-        // Check left
-        for (int i = index - 1; i >= Math.max(0, index - d); i--) {
-            if (arr[i] >= arr[index]) {
-                break;
-            }
-            maxJump = Math.max(maxJump, 1 + dfs(arr, d, i, dp));
-        }
-
-        // Check right
-        for (int i = index + 1; i <= Math.min(arr.length - 1, index + d); i++) {
-            if (arr[i] >= arr[index]) {
-                break;
-            }
-            maxJump = Math.max(maxJump, 1 + dfs(arr, d, i, dp));
-        }
-
-        dp[index] = maxJump;
-        return maxJump;
-    }
-}
