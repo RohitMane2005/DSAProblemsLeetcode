08d9d3ef3e7b944b5c25697991aa1bd8cfc74f1f
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-01 14:41:33 +0000

    [LeetCode] #0073 Set Matrix Zeroes (java)
    Problem:   https://leetcode.com/problems/set-matrix-zeroes/
    Runtime:   1 ms
    Memory:    47.6 MB
    Submitted: 2026-06-01 14:41:33 UTC

---
+++ Uncategorized/LeetCode_0073_Set_Matrix_Zeroes/solution.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean firstRowZeros = false;
+        boolean firstColZeros = false;
+
+        for(int j = 0; j< n; j++){
+            if(matrix[0][j] == 0){
+                firstRowZeros = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i< m; i++){
+            if(matrix[i][0] == 0){
+                firstColZeros = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+
+         for(int i = 1; i<m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstRowZeros){
+            for(int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstColZeros){
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+}
