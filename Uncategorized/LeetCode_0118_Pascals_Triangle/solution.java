8553de8a33e84aeef8dda8099722fba1d825cb96
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-02 06:10:11 +0000

    docs(0118): add complexity notes and finalize Pascal's Triangle
    Problem:      https://leetcode.com/problems/pascals-triangle/
    LeetCode-ID:  #0118 (java)
    Runtime:      1 ms
    Memory:       43.3 MB
    Submitted:    2026-06-02 06:10:11 UTC

--- Uncategorized/LeetCode_0118_Pascals_Triangle/solution.java
+++ Uncategorized/LeetCode_0118_Pascals_Triangle/solution.java
@@ -15,3 +15,21 @@ class Solution {
             List<Integer> prevRow = ans.get(i - 1);
             ArrayList<Integer> row = new ArrayList<>();
             
+            // Every row starts with 1
+            row.add(1);
+            
+            // Fill in the middle elements using elements from the previous row
+            for (int j = 0; j < prevRow.size() - 1; j++) {
+                row.add(prevRow.get(j) + prevRow.get(j + 1));
+            }
+            
+            // Every row ends with 1
+            row.add(1);
+            
+            // Add the completed row to our final list
+            ans.add(row);
+        }
+        
+        return ans;
+    }
+}
