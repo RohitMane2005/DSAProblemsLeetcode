ca3cb8ee3da06a04b704a0980166a75423e1e8b2
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-04 05:37:36 +0000

    Draft initial solution for Find the Duplicate Number
    Problem:      https://leetcode.com/problems/find-the-duplicate-number/
    LeetCode-ID:  #0287 (java)
    Runtime:      4 ms
    Memory:       82.8 MB
    Submitted:    2026-06-04 06:00:16 UTC

--- Uncategorized/LeetCode_0287_Find_the_Duplicate_Number/solution.java
+++ Uncategorized/LeetCode_0287_Find_the_Duplicate_Number/solution.java
@@ -9,15 +9,3 @@ class Solution {
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
-
-        // Phase 2: Find entrance of cycle
-        slow = nums[0];
-
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
-    }
-}
