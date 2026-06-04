8c4c284214a5bde942466c8b8b3e3fdd8b65c44b
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-04 06:00:14 +0000

    Draft initial solution for Find Missing and Repeated Values
    Problem:      https://leetcode.com/problems/find-missing-and-repeated-values/
    LeetCode-ID:  #3227 (java)
    Runtime:      1 ms
    Memory:       47.4 MB
    Submitted:    2026-06-04 06:18:01 UTC

--- Uncategorized/LeetCode_3227_Find_Missing_and_Repeated_Values/solution.java
+++ Uncategorized/LeetCode_3227_Find_Missing_and_Repeated_Values/solution.java
@@ -17,22 +17,3 @@ class Solution {
 
         while(i < size) {
             int correct = arr[i] - 1;
-
-            if(arr[i] != arr[correct]) {
-                int temp = arr[i];
-                arr[i] = arr[correct];
-                arr[correct] = temp;
-            } else {
-                i++;
-            }
-        }
-
-        for(i = 0; i < size; i++) {
-            if(arr[i] != i + 1) {
-                return new int[]{arr[i], i + 1};
-            }
-        }
-
-        return new int[]{-1, -1};
-    }
-}
