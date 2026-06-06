d8a3e8ee6e358b12f4595d8f2d84883938b03480
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-06 13:40:29 +0000

    [LeetCode] #0229 Majority Element II (java)
    Problem:   https://leetcode.com/problems/majority-element-ii/
    Runtime:   8 ms
    Memory:    58.7 MB
    Submitted: 2026-06-06 13:40:29 UTC

---
+++ Uncategorized/LeetCode_0229_Majority_Element_II/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int count = 1;
+        int n = nums.length;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                if (count > n / 3) {
+                    ans.add(nums[i - 1]);
+                }
+                count = 1;
+            }
+        }
+
+        // Check last element group
+        if (count > n / 3) {
+            ans.add(nums[n - 1]);
+        }
+
+        return ans;
+    }
+}
