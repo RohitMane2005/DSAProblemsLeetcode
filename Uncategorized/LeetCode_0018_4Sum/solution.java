f1cb460e55cfcd503fd88d7fceeb3cb60e872869
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-07 13:46:17 +0000

    feat(0018): draft initial solution for 4Sum
    Problem:      https://leetcode.com/problems/4sum/
    LeetCode-ID:  #0018 (java)
    Runtime:      20 ms
    Memory:       45.9 MB
    Submitted:    2026-06-07 14:05:41 UTC

--- Uncategorized/LeetCode_0018_4Sum/solution.java
+++ Uncategorized/LeetCode_0018_4Sum/solution.java
@@ -24,29 +24,3 @@ class Solution {
                     if (sum == target) {
 
                         ans.add(Arrays.asList(
-                            nums[i], nums[j],
-                            nums[left], nums[right]
-                        ));
-
-                        left++;
-                        right--;
-
-                        while (left < right &&
-                               nums[left] == nums[left - 1])
-                            left++;
-
-                        while (left < right &&
-                               nums[right] == nums[right + 1])
-                            right--;
-
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
-                }
-            }
-        }
-        return ans;
-    }
-}
