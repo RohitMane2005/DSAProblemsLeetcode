4e5fadb3d83c6c55978fba96da4b41ff352e45c2
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-09 07:59:18 +0000

    Finalize Reverse Linked List solution (java)
    Problem:      https://leetcode.com/problems/reverse-linked-list/
    LeetCode-ID:  #0206 (java)
    Runtime:      0 ms
    Memory:       44.4 MB
    Submitted:    2026-06-09 07:59:18 UTC

--- Uncategorized/LeetCode_0206_Reverse_Linked_List/solution.java
+++ Uncategorized/LeetCode_0206_Reverse_Linked_List/solution.java
@@ -10,3 +10,16 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next;
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
