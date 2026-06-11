d661a74fd49d3dfadbbdac8f225ec6a48a9af721
Rohit Mane <rohitbusiness9115@gmail.com>
2026-06-11 14:07:09 +0000

    Draft initial solution for Intersection of Two Linked Lists
    Problem:      https://leetcode.com/problems/intersection-of-two-linked-lists/
    LeetCode-ID:  #0160 (java)
    Runtime:      1 ms
    Memory:       52.6 MB
    Submitted:    2026-06-11 14:31:48 UTC

--- Uncategorized/LeetCode_0160_Intersection_of_Two_Linked_Lists/solution.java
+++ Uncategorized/LeetCode_0160_Intersection_of_Two_Linked_Lists/solution.java
@@ -10,15 +10,3 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode a = headA;
-        ListNode b = headB;
-
-        while (a != b) {
-            a = (a == null) ? headB : a.next;
-            b = (b == null) ? headA : b.next;
-        }
-
-        return a; // intersection node or null
-    }
-}
