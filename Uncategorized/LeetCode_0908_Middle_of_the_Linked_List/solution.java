66dc53120b86b0ee89eea7394263618f6474a22a
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-05 14:21:23 +0000

    feat(0908): draft initial solution for Middle of the Linked List
    Problem:      https://leetcode.com/problems/middle-of-the-linked-list/
    LeetCode-ID:  #0908 (java)
    Runtime:      0 ms
    Memory:       42.9 MB
    Submitted:    2026-07-05 14:44:14 UTC

--- Uncategorized/LeetCode_0908_Middle_of_the_Linked_List/solution.java
+++ Uncategorized/LeetCode_0908_Middle_of_the_Linked_List/solution.java
@@ -9,14 +9,3 @@
  * }
  */
 class Solution {
-    public ListNode middleNode(ListNode head) {
-       ListNode slow = head;
-       ListNode fast = head;
-
-       while(fast != null && fast.next != null){
-        fast = fast.next.next;
-        slow = slow.next;
-       }
-       return slow;
-    }
-}
