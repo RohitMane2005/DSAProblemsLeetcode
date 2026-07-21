bb72ed6cdf1ee5d955069605e183b41c7b906b44
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-21 10:08:27 +0000

    Initial solution implementation for #0070
    Problem:      https://leetcode.com/problems/climbing-stairs/
    LeetCode-ID:  #0070 (java)
    Runtime:      0 ms
    Memory:       41.9 MB
    Submitted:    2026-07-21 10:31:56 UTC

--- Uncategorized/LeetCode_0070_Climbing_Stairs/solution.java
+++ Uncategorized/LeetCode_0070_Climbing_Stairs/solution.java
@@ -5,11 +5,3 @@ class Solution {
         int[] dp = new int[n + 1]; 
         dp[1] = 1; 
         dp[2] = 2; 
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2]; 
-        } 
-
-        return dp[n]; 
-    } 
-}
