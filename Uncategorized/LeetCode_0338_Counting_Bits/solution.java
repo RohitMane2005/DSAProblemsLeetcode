f8af4a234179d558e103bb3e089647189afe6e2d
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-21 09:35:08 +0000

    Add initial approach for Counting Bits
    Problem:      https://leetcode.com/problems/counting-bits/
    LeetCode-ID:  #0338 (java)
    Runtime:      2 ms
    Memory:       48.5 MB
    Submitted:    2026-07-21 10:08:03 UTC

--- Uncategorized/LeetCode_0338_Counting_Bits/solution.java
+++ Uncategorized/LeetCode_0338_Counting_Bits/solution.java
@@ -3,9 +3,3 @@ class Solution {
         int[] ans = new int[n+1];
         ans[0] = 0;
         for(int i = 1; i<=n; i++){
-            if(i % 2 == 0) ans[i] = ans[i/2];
-            else ans[i] = ans[i/2]+1;
-        }
-        return ans;
-    }
-}
