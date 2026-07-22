c045c20481661891ed5a7da4103815684802efc7
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-22 09:35:09 +0000

    feat(0747): draft initial solution for Min Cost Climbing Stairs
    Problem:      https://leetcode.com/problems/min-cost-climbing-stairs/
    LeetCode-ID:  #0747 (java)
    Runtime:      0 ms
    Memory:       44.8 MB
    Submitted:    2026-07-22 10:20:10 UTC

--- Uncategorized/LeetCode_0747_Min_Cost_Climbing_Stairs/solution.java
+++ Uncategorized/LeetCode_0747_Min_Cost_Climbing_Stairs/solution.java
@@ -5,10 +5,3 @@ class Solution {
 
         for (int i = 2; i < cost.length; i++) {
             int current = cost[i] + Math.min(first, second);
-            first = second;
-            second = current;
-        }
-
-        return Math.min(first, second);
-    }
-}
