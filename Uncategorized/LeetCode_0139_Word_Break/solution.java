b18c10aeeb26db96676433e514d5296dca056283
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-25 04:10:38 +0000

    Refactor Word Break for better runtime performance
    Problem:      https://leetcode.com/problems/word-break/
    LeetCode-ID:  #0139 (java)
    Runtime:      8 ms
    Memory:       46.1 MB
    Submitted:    2026-07-25 04:24:50 UTC

--- Uncategorized/LeetCode_0139_Word_Break/solution.java
+++ Uncategorized/LeetCode_0139_Word_Break/solution.java
@@ -6,3 +6,12 @@ class Solution {
 
         for(int i = 1; i <=s.length(); i++){
             for(int j = 0; j<i; j++){
+                if(dp[j] && wordSet.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
