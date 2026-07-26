3d4441a872b0f22dd7fe95ff57e6162178804d78
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-26 14:35:36 +0000

    [LeetCode] #0516 Longest Palindromic Subsequence (java)
    Problem:   https://leetcode.com/problems/longest-palindromic-subsequence/
    Runtime:   29 ms
    Memory:    42.9 MB
    Submitted: 2026-07-26 14:35:36 UTC

---
+++ Uncategorized/LeetCode_0516_Longest_Palindromic_Subsequence/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[] dp = new int[n];
+
+        // Process subproblems from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1; // Base case: single character is a palindrome of length 1
+            int prev = 0; // Stores dp[i+1][j-1] from the 2D DP grid
+            
+            for (int j = i + 1; j < n; j++) {
+                int temp = dp[j];
+                
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[j] = prev + 2;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+                
+                prev = temp;
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
