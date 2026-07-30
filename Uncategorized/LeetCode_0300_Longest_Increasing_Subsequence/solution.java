306adfefb67b6da6b7feefdc63e0e136a8a34e10
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-30 03:32:36 +0000

    Draft initial solution for Longest Increasing Subsequence
    Problem:      https://leetcode.com/problems/longest-increasing-subsequence/
    LeetCode-ID:  #0300 (java)
    Runtime:      38 ms
    Memory:       46 MB
    Submitted:    2026-07-30 04:06:17 UTC

--- Uncategorized/LeetCode_0300_Longest_Increasing_Subsequence/solution.java
+++ Uncategorized/LeetCode_0300_Longest_Increasing_Subsequence/solution.java
@@ -12,17 +12,3 @@ class Solution {
         for (int i = n - 1; i >= 0; i--) {
             // Base case: at minimum, a subsequence of length 1 starts at index i
             dp[i] = 1;
-
-            // Look forward at all elements that come after index i
-            for (int j = i + 1; j < n; j++) {
-                if (nums[j] > nums[i]) {
-                    dp[i] = Math.max(dp[i], 1 + dp[j]);
-                }
-            }
-
-            maxLength = Math.max(maxLength, dp[i]);
-        }
-
-        return maxLength;
-    }
-}
