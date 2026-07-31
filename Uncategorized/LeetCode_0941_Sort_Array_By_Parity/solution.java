2cfb1ed8502a83f72fcb6587342e10abb6700444
Rohit Mane <rohitbusiness9115@gmail.com>
2026-07-31 06:19:22 +0000

    Improve efficiency and reduce memory usage in Sort Array By Parity
    Problem:      https://leetcode.com/problems/sort-array-by-parity/
    LeetCode-ID:  #0941 (java)
    Runtime:      0 ms
    Memory:       46.7 MB
    Submitted:    2026-07-31 06:33:06 UTC

--- Uncategorized/LeetCode_0941_Sort_Array_By_Parity/solution.java
+++ Uncategorized/LeetCode_0941_Sort_Array_By_Parity/solution.java
@@ -17,3 +17,23 @@ class Solution {
         }
         return nums;
     }
+}
+
+// class Solution {
+//     public int[] sortArrayByParity(int[] arr) {
+//         int n = arr.length;
+//         int even = 0;
+//         int odd = n-1;
+
+//         while(even <=odd){
+//             if(arr[even]%2!=0){
+//                 int temp = arr[even];
+//                 arr[even] = arr[odd];
+//                 arr[odd] = temp;
+//                 odd--;
+//             }
+//             else even++;
+//         }
+//         return arr;
+//     }
+// }
