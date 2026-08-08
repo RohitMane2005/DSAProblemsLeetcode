c8f4f615d0893acc77a0befa710ee966e878e88e
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-08 14:35:24 +0000

    Optimize time and space complexity for Valid Palindrome
    Problem:      https://leetcode.com/problems/valid-palindrome/
    LeetCode-ID:  #0125 (java)
    Runtime:      2 ms
    Memory:       43.8 MB
    Submitted:    2026-08-08 14:54:37 UTC

--- Uncategorized/LeetCode_0125_Valid_Palindrome/solution.java
+++ Uncategorized/LeetCode_0125_Valid_Palindrome/solution.java
@@ -11,3 +11,17 @@ class Solution {
 
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != 
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
