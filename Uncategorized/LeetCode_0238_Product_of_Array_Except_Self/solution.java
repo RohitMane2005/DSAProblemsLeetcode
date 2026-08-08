10bd4c581ac6cb921b39e66fe27e590eae1960db
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-08 04:00:25 +0000

    [LeetCode] #0238 Product of Array Except Self (java)
    Problem:   https://leetcode.com/problems/product-of-array-except-self/
    Runtime:   2 ms
    Memory:    72.2 MB
    Submitted: 2026-08-08 04:00:25 UTC

--- Uncategorized/LeetCode_0238_Product_of_Array_Except_Self/solution.java
+++ Uncategorized/LeetCode_0238_Product_of_Array_Except_Self/solution.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int ans[] = new int[n];
+        int[] ans = new int[n];
+
+        // Step 1: Calculate prefix products for each index
+        // ans[i] will contain the product of all elements to the left of i
         ans[0] = 1;
-        for(int i = 1; i<n; i++){
-            ans[i] = ans[i-1] * nums[i -1];
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
 
-        int suffix = 1;
-        for(int i = n-1; i>= 0; i--){
-            ans[i] = ans[i] * suffix;
-            suffix *= nums[i];
+        // Step 2: Calculate suffix products on the fly and multiply with prefix product
+        int right = 1; // Tracks the product of all elements to the right of i
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * right;
+            right *= nums[i];
         }
+
         return ans;
     }
 }
