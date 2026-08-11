eaf4b149fc1a59b364c979702dc39099558ae18f
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-11 13:06:40 +0000

    feat(0042): draft initial solution for Trapping Rain Water
    Problem:      https://leetcode.com/problems/trapping-rain-water/
    LeetCode-ID:  #0042 (java)
    Runtime:      0 ms
    Memory:       47.8 MB
    Submitted:    2026-08-11 13:32:22 UTC

--- Uncategorized/LeetCode_0042_Trapping_Rain_Water/solution.java
+++ Uncategorized/LeetCode_0042_Trapping_Rain_Water/solution.java
@@ -13,18 +13,3 @@ class Solution {
                 else{
                     water += leftMax - height[left];
                 }
-                left++;
-            }else{
-                if(height[right] >= rightMax){
-                    rightMax = height[right];
-                }
-                else{
-                    water += rightMax - height[right];
-                }
-                right--;
-            }
-
-        }
-        return water;
-    }
-}
