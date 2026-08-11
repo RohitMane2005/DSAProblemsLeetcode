7efadff15a89d66666a78824932209dffc8114b0
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-11 05:05:57 +0000

    Refactor Container With Most Water for better runtime performance
    Problem:      https://leetcode.com/problems/container-with-most-water/
    LeetCode-ID:  #0011 (java)
    Runtime:      5 ms
    Memory:       77.4 MB
    Submitted:    2026-08-11 05:18:08 UTC

--- Uncategorized/LeetCode_0011_Container_With_Most_Water/solution.java
+++ Uncategorized/LeetCode_0011_Container_With_Most_Water/solution.java
@@ -6,3 +6,12 @@ class Solution {
         while(left < right){
             int currentMax = Math.min(height[left],height[right]) * (right-left);
             max = Math.max(currentMax,max);
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return max;
+    }
+}
