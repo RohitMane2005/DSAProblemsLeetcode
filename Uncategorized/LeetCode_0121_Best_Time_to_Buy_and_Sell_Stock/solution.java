f1decc8af53a00eeadaa71c922a3f370a62c2044
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-11 13:50:53 +0000

    docs(0121): add complexity notes and finalize Best Time to Buy and Sell Stock
    Problem:      https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
    LeetCode-ID:  #0121 (java)
    Runtime:      1 ms
    Memory:       94.5 MB
    Submitted:    2026-08-11 13:50:53 UTC

--- Uncategorized/LeetCode_0121_Best_Time_to_Buy_and_Sell_Stock/solution.java
+++ Uncategorized/LeetCode_0121_Best_Time_to_Buy_and_Sell_Stock/solution.java
@@ -8,3 +8,13 @@ class Solution {
         int maxProfit = 0;
 
         for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } 
+            else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
