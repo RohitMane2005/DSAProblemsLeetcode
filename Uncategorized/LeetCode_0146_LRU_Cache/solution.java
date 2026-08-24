081305d985e9d7a401800973f07b9c8b0f47f71c
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-24 09:48:52 +0000

    Improve efficiency and reduce memory usage in LRU Cache
    Problem:      https://leetcode.com/problems/lru-cache/
    LeetCode-ID:  #0146 (java)
    Runtime:      13 ms
    Memory:       58.6 MB
    Submitted:    2026-08-24 10:03:45 UTC

--- Uncategorized/LeetCode_0146_LRU_Cache/solution.java
+++ Uncategorized/LeetCode_0146_LRU_Cache/solution.java
@@ -39,3 +39,44 @@ class LRUCache {
         moveToHead(node);
         return node.value;
     }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            addToHead(newNode);
+
+            if (map.size() > capacity) {
+                Node lru = removeTail();
+                map.remove(lru.key);
+            }
+        }
+    }
+
+    private void addToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private Node removeTail() {
+        Node lru = tail.prev;
+        removeNode(lru);
+        return lru;
+    }
+}
