618fc33301d7bdabb13963672f806f94da7c5b25
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-24 09:50:13 +0000

    Optimize solution using efficient data structures
    Problem:      https://leetcode.com/problems/merge-intervals/
    LeetCode-ID:  #0056 (java)
    Runtime:      8 ms
    Memory:       49 MB
    Submitted:    2026-08-24 10:03:59 UTC

--- Uncategorized/LeetCode_0056_Merge_Intervals/solution.java
+++ Uncategorized/LeetCode_0056_Merge_Intervals/solution.java
@@ -16,3 +16,22 @@ class Solution {
         result.add(currentInterval);
 
         // 3. Iterate through the rest of the intervals and merge if they overlap
+        for (int i = 1; i < intervals.length; i++) {
+            int currentEnd = currentInterval[1];
+            int nextStart = intervals[i][0];
+            int nextEnd = intervals[i][1];
+
+            if (nextStart <= currentEnd) { 
+                // Overlap detected: Update the end of the current interval
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else {
+                // No overlap: Move to the next interval and add it to the list
+                currentInterval = intervals[i];
+                result.add(currentInterval);
+            }
+        }
+
+        // Convert the list back to a 2D array
+        return result.toArray(new int[result.size()][]);
+    }
+}
