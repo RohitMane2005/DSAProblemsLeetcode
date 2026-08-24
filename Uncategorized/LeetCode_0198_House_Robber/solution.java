f5cced684007ef306e9a3a76b35c0fa28ac032c3
Rohit Mane <rohitbusiness9115@gmail.com>
2026-08-24 09:31:12 +0000

    Add initial approach for House Robber
    Problem:      https://leetcode.com/problems/house-robber/
    LeetCode-ID:  #0198 (java)
    Runtime:      0 ms
    Memory:       42.8 MB
    Submitted:    2026-08-24 10:04:11 UTC

--- Uncategorized/LeetCode_0198_House_Robber/solution.java
+++ Uncategorized/LeetCode_0198_House_Robber/solution.java
@@ -5,12 +5,3 @@ class Solution {
 
         int first = nums[0];
         int second = Math.max(nums[0],nums[1]);
-
-        for(int i = 2; i<nums.length; i++){
-            int current = Math.max(nums[i] + first,second);
-            first = second;
-            second = current;
-        }
-        return second;
-    }
-}
